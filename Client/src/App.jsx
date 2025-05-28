8e27327a94f86b40d55120be8dfa2f8310881be0
RajyavardhanSingh <rajyavardhansing2003@gmail.com>
2025-05-29 01:06:46 +0530

    first commit

---
+++ Client/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='text-amber-400 text-9xl flex justify-center items-center h-screen' >
+      Hello world
+    </div>
+  )
+}
+
+export default App
